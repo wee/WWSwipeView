bc61e36fe4b09c36318234584d6a863df42287e6
Wee W <weerasak@gmail.com>
2013-02-23 13:28:26 -0600

    Initial Commit

---
+++ WeeSwipeView/WWViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWViewController.h
+//  WeeSwipeView
+//
+//  Created by Wee Witthawaskul on 2/23/13.
+//  Copyright (c) 2013 Wee Witthawaskul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWViewController : UIViewController
+
+@end
---
+++ WeeSwipeView/WWViewController.m
@@ -0,0 +1,29 @@
+//
+//  WWViewController.m
+//  WeeSwipeView
+//
+//  Created by Wee Witthawaskul on 2/23/13.
+//  Copyright (c) 2013 Wee Witthawaskul. All rights reserved.
+//
+
+#import "WWViewController.h"
+
+@interface WWViewController ()
+
+@end
+
+@implementation WWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
